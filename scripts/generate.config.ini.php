ee07c5e3c7c6614eb8852195cb68d6919621ea56
Jonathan Perret <j-github@jonathanperret.net>
2023-01-05 20:26:51 +0100

    Configure SMTP email

--- scripts/generate.config.ini.php
+++ scripts/generate.config.ini.php
@@ -6,13 +6,3 @@ ob_start();
 eval($content);
 $content = ob_get_clean();
 file_put_contents(__DIR__.'/config.ini.php', $content);
-
-/*
-if (getenv('MAIL_URL') !== False) {
-  $content = file_get_contents(__DIR__.'/config-email.ini.php.tmpl');
-  ob_start();
-  eval($content);
-  $content = ob_get_contents();
-  file_put_contents(__DIR__.'/config.ini.php', $content, FILE_APPEND);
-}
- */
